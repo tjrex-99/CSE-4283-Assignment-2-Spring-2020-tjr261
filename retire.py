80c7ae3624607f2a748488a6786ae1105a40caa8
tjrex-99 <57365711+tjrex-99@users.noreply.github.com>
2020-02-08 21:28:18 -0600

    CLI finished

--- retire.py
+++ retire.py
@@ -1,3 +1,7 @@
+# CSE 4283 Software Testing and QA Assignment 2
+# by Timothy Reaux
+# retire.py
+
 # function for calculating retirement age (or 'Not possible') based on inputted current age, annual salary, percentage saved, and savings goal
 def retire(age, salary, percentage, goal):
     yearly = (salary * percentage) * 1.35
@@ -6,6 +10,6 @@ def retire(age, salary, percentage, goal):
     r_age = age + save_time
 
     if r_age >= 100:
-        return ['Not possible', r_age]
+        return 'Not possible'
 
     return r_age
